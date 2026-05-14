8a0cb1453b01032d913013d0d8b58db799f29ee1
Scott Collins <scottjcollins19@gmail.com>
2026-05-14 21:57:20 +0100

    Completed Source Code Layout

---
+++ main.py
@@ -0,0 +1,10 @@
+from src.mission_analysis import MissionAnalysis
+from src.conversions import Conversions
+from src.data_import import Data
+from src.raymer_equations import RaymerEquations
+from src.raymer_variables import RaymerVariables
+
+def main(mtow, mf, pl):
+
+
+# iteration(50000, 10700, 14341) 
