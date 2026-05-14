173cd60a1a9615e27bba9dec046e6cd8da974003
Scott Collins <scottjcollins19@gmail.com>
2026-05-14 21:33:37 +0100

    Data importation Added

--- src/raymer_equations.py
+++ src/raymer_equations.py
@@ -8,7 +8,7 @@ class RaymerEquations:
         self.W_horizontal_tail = 3.316*(1+(self.rvs.Fw/self.rvs.Bh))**-2*(self.rvs.Wto*self.rvs.Nz/1000)**0.26*self.rvs.Sht**0.806
         self.W_vertical_tail = 0.452*self.rvs.Krht*(1+self.rvs.Ht/self.rvs.Hv)**0.5*(self.rvs.Wto*self.rvs.Nz)**0.488*self.rvs.Svt**0.718*self.rvs.M**0.341*self.rvs.Lt**-1*(1+self.rvs.Sr/self.rvs.Svt)**0.348*self.rvs.Avt**0.223*(1+self.rvs.lambdaa)**0.25*(np.cos(0))**-0.323
         self.W_fuselage = 0.499*self.rvs.kdw*self.rvs.Wto**0.35*self.rvs.Nz**0.25*self.rvs.L*0.5*self.rvs.D**0.849*self.rvs.W**0.685
-        self.W_main_landing_gear = self.rvs.Kcb*self.rvs.Ktpg*(self.rvs.Wl*self.rvs.Nl)**0.25*Lm**0.973
+        self.W_main_landing_gear = self.rvs.Kcb*self.rvs.Ktpg*(self.rvs.Wl*self.rvs.Nl)**0.25*self.rvs.Lm**0.973
         self.W_nose_landing_gear = (self.rvs.Wl/self.rvs.Nl)**0.290*self.rvs.Ln**0.5*self.rvs.Nnw**0.525
         self.W_engine = 0.01*self.rvs.Wen**0.717*self.rvs.Nen*self.rvs.Nz
         self.W_fuel_system = 7.45*self.rvs.Vt**0.47*(1+self.rvs.Vi/self.rvs.Vt)**-0.095*(1+self.rvs.Vp/self.rvs.Vt)*self.rvs.Nt**0.066*self.rvs.Nen**0.052*(self.rvs.T*self.rvs.SFC/1000)**0.249
